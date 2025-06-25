57f75a4be564624b0dc39ee38a43feea17555daf
Felipe Valboeno <feliperocha14@hotmail.com>
2025-06-25 14:51:26 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/example/projeto_tdd/ProjetoTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
